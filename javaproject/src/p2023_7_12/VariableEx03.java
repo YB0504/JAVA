d43e9247385a83fc12201e5bb952bef1c5a21724
yeo9411 <yeo9411@naver.com>
2023-07-13 18:26:48 +0900

    first commit

---
+++ javaproject/src/p2023_7_12/VariableEx03.java
@@ -0,0 +1,19 @@
+package p2023_7_12;
+
+//이스케이프 문자 예제
+public class VariableEx03 {
+
+	public static void main(String[] args) {
+		
+		System.out.println("번호\t이름\t나이"); // tap 만큰 띄우기 두번 쓸때는 \t\t
+
+		System.out.print("행 단위 출력\n"); // 줄 바꿈 \t와 동일하게 사용 시 두줄 변경
+
+		System.out.print("행 단위 출력\n"); // 줄 바꿈
+
+		System.out.println("우리는 \"개발자\"입니다"); // " 출력 \를 쓰지않고 ""를 더 사용하면 중복으로 오류
+
+		System.out.println("봄\\여름\\가을\\겨울"); // \ 출력
+	}
+
+}
---
+++ javaproject/src/p2023_7_13/Oper05.java
@@ -0,0 +1,33 @@
+package p2023_7_13;
+
+import java.util.Scanner;
+
+// api 찾아보기
+public class Oper05 {
+
+	public static void main(String[] args) {
+
+// 예비처리를 하지않아도 되는 클래스 (import 필요)
+// java.io.* 패키지에 있는 클래스로도 가능하나 예비처리가 필요
+// Scanner 객체를 생성 해야만 입력장치로 받을 수 있음
+		
+		System.out.println("정수 2개를 입력하세요?");
+		// 출럭 System.out.
+		// 표준입력 장치인 키보드로 입력을 받기 위해서 Scanner 객체 생성
+		Scanner sc = new Scanner(System.in);
+		// 입력을 받아야 하기 때문에 (System.in) - 정적 필드
+		// 정수형태로 입력을 받기 위해서 nextInt() 메소드로 입력을 받는다.
+		int n1 = sc.nextInt(); // 스페이스바 or 엔터키로 구분함 (콘솔에 입력
+		int n2 = sc.nextInt();
+		System.out.println("n1 : " +n1);
+		System.out.println("n2 : " +n2);
+		
+		if(n1 > n2 ) { // 조건식이 참이면 if문 아랫쪽을 실행
+			System.out.println(n1 + "이" + n2 + "보다 크다");
+		}else {			// 조건식이 거짓이면 else 아랫쪽을 실행
+			System.out.println(n2 + "이" + n1 + "보다 크다");			
+		}
+		
+	}
+
+}
