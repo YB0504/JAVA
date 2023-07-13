9fd0dd57bb81ec6ad42ec6034accb0c7d4e70b59
YB0504 <139450364+YB0504@users.noreply.github.com>
2023-07-13 21:52:44 +0900

    first github

---
+++ javaproject/src/p2023_7_13/Reminding03.java
@@ -0,0 +1,29 @@
+package p2023_7_13;
+
+public class Reminding03 {
+
+	public static void main(String[] args) {
+		
+		// 기본 타입과 문자열 간의 변환
+		
+		// 문자열을 기본타입으로 강제 변환
+		int i1 = Integer.parseInt("10");
+		double b2 = Double.parseDouble("3.14");
+		boolean b3 = Boolean.parseBoolean("true");
+		
+		System.out.println("i1 : " + i1);
+		System.out.println("b2 : " + b2);
+		System.out.println("b3 : " + b3);
+		
+		// 기본타입을 문자열로 강제 변환
+		String s1 = String.valueOf(10);
+		String s2 = String.valueOf(3.14);
+		String s3 = String.valueOf(true);
+		
+		System.out.println("s1 : " + s1);
+		System.out.println("s2 : " + s2);
+		System.out.println("s3 : " + s3);
+		
+	}
+
+}
