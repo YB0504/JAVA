cdae89681b79b1a84936458037b047786ddd0311
YB0504 <139450364+YB0504@users.noreply.github.com>
2023-07-18 22:29:45 +0900

    Today' class Self Study

---
+++ javaproject/src/p2023_7_18/SelfStudy01.java
@@ -0,0 +1,25 @@
+package p2023_7_18;
+
+import java.util.Scanner;
+
+public class SelfStudy01 {
+
+	public static void main(String[] args) {
+
+//	5과목의 점수의 총합을 구하시오
+		
+		System.out.println("각 과목의 점수를 입력하시오.");
+		Scanner sc = new Scanner(System.in);
+		
+		int[] score = new int[5]; 
+		
+		int sum = 0;
+		for(int i = 0; i < score.length; i++) {
+			score[i] = sc.nextInt();
+			sum += score[i]; // sum = sum + i
+		}System.out.println("총합 : " + sum);
+			double avg = (double) sum / 5;
+			System.out.printf("평균 : %.1f", + avg);
+	}
+
+}
