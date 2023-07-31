3620f2bf590788e8e7edab4be2088fa1ce22da70
yeo9411 <yeo9411@naver.com>
2023-07-31 17:39:31 +0900

    Today's JAVA Class Day 17

--- javaproject/src/p2023_7_31/RefTest02.java
+++ javaproject/src/p2023_7_31/RefTest02.java
@@ -1,12 +1,18 @@
-package p2023_7_28;
+package p2023_7_31;
 
-class Parent02 {
+// * 다운 캐스팅(강제 형변환)
+// 1. 슈퍼클래스에서 서브클래스로 형변환 하는것
+// 2. 참조 가능한 영역이 확대가 된다.
+// 3. 컴파일러에 의해서 암시적 형변환(자동 형변환)이 되지 않기    
+//    때문에 자료형을 생략할 수 없다.(강제 형변환)
+
+class Parent02 {						// 부모 클래스
 	public void parentPrn() {
 		System.out.println("슈퍼 클래스 : ParentPrn 메서드");
 	}
 }
 
-class Child02 extends Parent02 {
+class Child02 extends Parent02 {		// 자식 클래스
 	public void childPrn() {
 		System.out.println("서브 클래스 : ChildPrn 메서드");
 	}
