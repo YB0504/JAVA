4e7f3b29a1ae4b46bd0e9babcf6a56f1febf882a
yeo9411 <yeo9411@naver.com>
2023-10-30 17:59:41 +0900

    MyBatis 게시판 개인 연습

---
+++ memberPr/src/main/java/controller/MemberController.java
@@ -0,0 +1,68 @@
+package controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import service.Action;
+import service.ActionForward;
+
+@WebServlet("*.do")
+public class MemberController extends HttpServlet {
+
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		// 클라이언트가 요청한 전체 경로값을 구한다.
+		String requestURI = request.getRequestURI();
+		// 현재 프로젝트명을 구한다.
+		String contextPath = request.getContextPath();
+		// 현재 프로젝트명의 길이만큼의 인덱스 번호부터 추출한다.(요청한 이름값)
+		String command = requestURI.substring(contextPath.length());
+
+		System.out.println("requestURI : " + requestURI);
+		System.out.println("contextPath : " + contextPath);
+		System.out.println("command : " + command);
+		
+		Action action = null;
+		ActionForward forward = null;
+		
+		if(command.equals("/MemberInsert.do")) {
+			// 예외 처리
+			try {
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+		if(forward != null) {
+			if(forward.isRedirect()) {
+				response.sendRedirect(forward.getPath());
+			}else {
+				RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
+				dispatcher.forward(request, response);
+			}
+		}
+
+	}
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("get");
+		doProcess(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println("post");
+		doProcess(request, response);
+	}
+
+}
