44d10eee091e84962e4511d87a9b17a262190114
yeo9411 <yeo9411@naver.com>
2023-11-03 16:52:08 +0900

    백준 문제풀이

--- BaekJoon/src/Solve/Main.java
+++ BaekJoon/src/Solve/Main.java
@@ -1,17 +1,28 @@
 package Solve;
 
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int h = sc.nextInt();
+		int m = sc.nextInt();
+		sc.close();
 		
-		System.out.println("|\\_/|");
-		System.out.println("|q p|   /}");
-		System.out.println("( 0 )\"\"\"\\");
-		System.out.println("|\"^\"`    |");
-		System.out.println("||_/=\\\\__|");
-		
+		// 주어진 분이 45분보다 작을시
+		if (m < 45) {
+			h--;
+			m = 60 - (45 - m);
+			
+			// 주어진 시간에서 -1일 때 시간이 0보다 작다면
+			if (h < 0) {
+				h = 23;
+			}
+			System.out.println(h + " " + m);
+		}
+		System.out.println(h + " " + (m - 45));
 	}
 
 }
